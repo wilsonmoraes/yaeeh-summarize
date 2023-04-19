9780c502caca45e136bad9b2d0fe6b2dcce79e3b
Wilson Moraes dos Santos <wilsontads@gmail.com>
2023-04-19 16:33:24 -0300

    add requirements

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/getmsg/', methods=['GET'])
+def respond():
+    # Retrieve the name from the url parameter /getmsg/?name=
+    name = request.args.get("name", None)
+
+    # For debugging
+    print(f"Received: {name}")
+
+    response = {}
+
+    # Check if the user sent a name at all
+    if not name:
+        response["ERROR"] = "No name found. Please send a name."
+    # Check if the user entered a number
+    elif str(name).isdigit():
+        response["ERROR"] = "The name can't be numeric. Please send a string."
+    else:
+        response["MESSAGE"] = f"Welcome {name} to our awesome API!"
+
+    # Return the response in json format
+    return jsonify(response)
 
 
 if __name__ == '__main__':
-    app.run()
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
