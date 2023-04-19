cccc77776d547dad24699687f82be46acc0d47b6
Wilson Moraes dos Santos <wilsontads@gmail.com>
2023-04-19 16:38:12 -0300

    add requirements

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
+@app.route("/")
+def home():
+    return "Hello World!"
+
+
 @app.route('/getmsg/', methods=['GET'])
 def respond():
     # Retrieve the name from the url parameter /getmsg/?name=
