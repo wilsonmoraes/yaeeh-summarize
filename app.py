e15035d0650088b805f5ec060f28081be24dcc4e
Wilson Moraes dos Santos <wilsontads@gmail.com>
2023-04-19 21:39:32 -0300

    add requirements

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
+import re
+import textwrap
+from time import time, sleep
+from urllib.parse import parse_qs
+from urllib.parse import urlparse
+
+import openai
+from flask import Flask, request, jsonify
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import TextFormatter
 
-from flask import Flask, request, jsonify
-
-from urllib.parse import urlparse
-from urllib.parse import parse_qs
-
-import textwrap
-import re
-import openai
-
 OPENAI_API_KEY = "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"
 OPENAI_ORGANIZATION = "org-hdnbzGyFPauPuAOCrcVrsVCS"
-PROMPT_STRING = "Write a detailed summary of the following:\n\n<<SUMMARY>>\n"
+PROMPT_STRING = "Escreva um resumo detalhado do seguinte vídeo:\n\n<<SUMMARY>>\n"
+openai.organization = OPENAI_ORGANIZATION
+openai.api_key = OPENAI_API_KEY
+
+openai.Model.list()
 
 app = Flask(__name__)
 
@@ -26,65 +29,77 @@ def home():
 def respond():
     # Retrieve the name from the url parameter /getmsg/?name=
     url = request.args.get("url", None)
-    parsed_url = urlparse(url)
-    video_id = parse_qs(parsed_url.query)['v'][0]
+    # Download transcript
+    video_id, text = get_transcript(url)
+    json_resp = {}
 
-    openai.organization = OPENAI_ORGANIZATION
-    openai.api_key = OPENAI_API_KEY
-
-    openai.Model.list()
-
-    # Get transcript for given YouTube video id
-
-    transcript = YouTubeTranscriptApi.get_transcript(video_id=video_id, languages=['en', 'pt'],
-                                                     preserve_formatting=True)
-
-    # Format transcript using TextFormatter from youtube_transcript_api library
-    formatter = TextFormatter()
-    transcript = formatter.format_transcript(transcript)
-
-    video_length = len(transcript)
-
-    # If the video is ~25 minutes or more, double the chunk size
-    # This is done to reduce overall amount of API calls
-    chunk_size = 4000 if video_length >= 25000 else 2000
-
-    # Wrap the transcript in chunks of characters
-    chunks = textwrap.wrap(transcript, chunk_size)
-
-    summaries = list()
-
-    # For each chunk of characters, generate a summary
-    for chunk in chunks:
-        prompt = PROMPT_STRING.replace("<<SUMMARY>>", chunk)
-
-        # Generate summary using GPT-3
-        # If the davinci model is incurring too much cost,
-        # the text-curie-001 model may be used in its place.
-        response = openai.Completion.create(
-            model="text-davinci-003", prompt=prompt, max_tokens=256
-        )
-        summary = re.sub("\s+", " ", response.choices[0].text.strip())
-        summaries.append(summary)
-
-    # Join the chunk summaries into one string
-    chunk_summaries = " ".join(summaries)
-    prompt = PROMPT_STRING.replace("<<SUMMARY>>", chunk_summaries)
-
-    # Generate a final summary from the chunk summaries
-    response = openai.Completion.create(
-        model="text-davinci-003", prompt=prompt, max_tokens=2056
-    )
-    final_summary = re.sub("\s+", " ", response.choices[0].text.strip())
-
-    # Print out all of the summaries
-    # for idx, summary in enumerate(summaries):
-    #    print(f"({idx}) - {summary}\n")
-
-    json_resp = {'final_summary': final_summary}
+    # Summarize the transcript (chunk by chunk if needed)
+    if text:
+        # Summarize transcript
+        results = ask_gpt(text, 'SUMMARY')
+        json_resp['summary'] = '\n\n'.join(results)
 
     # Return the response in json format
     return jsonify(json_resp)
+
+
+def get_transcript(url):
+    url_data = urlparse(url)
+    video_id = parse_qs(url_data.query)["v"][0]
+    if not video_id:
+        print('Video ID not found.')
+        return None
+
+    try:
+        formatter = TextFormatter()
+
+        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en', 'pt'])
+        text = formatter.format_transcript(transcript)
+        text = re.sub('\s+', ' ', text).replace('--', '')
+        return video_id, text
+
+    except Exception as e:
+        print('Error downloading transcript:', e)
+        return None
+
+
+def gpt3_completion(prompt, tokens=500):
+    max_retry = 3
+    retry = 0
+    while True:
+        try:
+            response = openai.Completion.create(
+                model="text-davinci-003",
+                prompt=prompt,
+                max_tokens=tokens)
+            text = response.choices[0].text.strip()
+            text = re.sub('\s+', ' ', text)
+            return text
+
+        except Exception as e:
+            retry += 1
+            if retry >= max_retry:
+                return "GPT3 error: %s" % e
+            print('Error communicating with OpenAI:', e)
+            sleep(1)
+
+
+def ask_gpt(text, job='SUMMARY'):
+    # Summarize chunks
+    chunks = textwrap.wrap(text, width=10000)
+    results = list()
+    for i, chunk in enumerate(chunks):
+        prompt = PROMPT_STRING.replace('<<SUMMARY>>', chunk)
+        prompt = prompt.encode(encoding='ASCII', errors='ignore').decode()
+        output = ''
+        if job == 'SUMMARY':
+            output = gpt3_completion(prompt, tokens=500)
+        elif job == 'REWRITE':
+            output = gpt3_completion(prompt, tokens=2048)
+        results.append(output)
+        print(f'{i + 1} of {len(chunks)}\n{output}\n\n\n')
+
+    return results
 
 
 if __name__ == '__main__':
